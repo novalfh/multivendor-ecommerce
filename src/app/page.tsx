85b148c501fddf4f6ef88ab090dff829a8ee0730
voeya <a@>
2025-05-07 19:44:13 +0700

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
       <Link href='/about'>
         <Button variant={'default'} className="btn" onClick={() => {
           
-        }}>Hello</Button>
+        }}>Submit</Button>
       </Link>
     </div>
   );
