b5921dbf4dde1f09b3080f3c1288014d3516d102
voeya <a@>
2025-05-07 19:27:38 +0700

    add shadcn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,17 @@
 // import Image from "next/image";
+import Link from "next/link";
+
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
     <div className="roots">
       <h1>hello</h1>
+      <Link href='/about'>
+        <Button variant={'default'} className="btn" onClick={() => {
+          
+        }}>Hello</Button>
+      </Link>
     </div>
   );
 }
